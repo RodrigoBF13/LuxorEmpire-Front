ec20010ead8bcb5a43d784fb432f2ee38dd8128d
Arthur <arthurhenrique0708@gmail.com>
2023-09-19 07:47:58 -0300

    .

--- site/src/pages/adm/adm-pedidos-concluidos/index.js
+++ site/src/pages/adm/adm-pedidos-concluidos/index.js
@@ -1,2 +1,12 @@
 import './index.scss';
 
+import CompMenuBar from '../../../components/adm/menubar';
+
+export default function AdmPedidosConcluidos() {
+
+    return(
+    <div className="adm-pagina-pedidos-concluidos">
+
+    </div>
+    )
+}
